5bf075422beee935f9cc2bc21daee9ac96370c4f
AcirDeveloper <acirdeveloper@gmail.com>
2022-12-08 21:45:45 -0500

    dp

--- 03-et-rickandmorty-app/src/App.jsx
+++ 03-et-rickandmorty-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Location from './components/Location'
+// importar axios
 import axios from 'axios'
 import './App.css'
 import Search from './components/Search'
