888bac4527d7f18df118c86795cfc94f01d53130
AcirDeveloper <acirdeveloper@gmail.com>
2022-12-09 23:59:23 -0500

    crud

---
+++ 04-et-crud-app/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import UsersForm from './components/UsersForm'
+import UsersList from './components/UsersList'
+import axios from 'axios'
+import Modal from './components/Modal'
+// import image from './assets/scattered-forcefields.svg'
+
+function App() {
+    const [users, setUsers] = useState([])
+    const [active, setActive] = useState(false)
+    const [editSelected, setEditSelected] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://users-crud1.herokuapp.com/users/').then((res) => setUsers(res.data))
+    }, [])
+
+    const getUsers = () => {
+        axios.get('https://users-crud1.herokuapp.com/users/').then((res) => setUsers(res.data))
+    }
+
+    const selectUser = (user) => {
+        setEditSelected(user)
+        toggle()
+    }
+
+    const deselectUser = () => setEditSelected(null)
+
+    const toggle = () => {
+        setActive(!active)
+    }
+
+    document.body.style = `background: #e5c0c0`
+
+    return (
+        <div className='App'>
+            <div className='header'>
+                <button onClick={toggle}>Create User</button>
+                <span></span>
+            </div>
+            <Modal active={active} toggle={toggle}>
+                <UsersForm
+                    getUsers={getUsers}
+                    toggle={toggle}
+                    editSelected={editSelected}
+                    deselectUser={deselectUser}
+                />
+            </Modal>
+            <UsersList users={users} selectUser={selectUser} toggle={toggle} getUsers={getUsers} />
+        </div>
+    )
+}
+
+export default App
---
+++ 04-et-crud-app/src/components/UsersList.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import React from 'react'
+
+const UsersList = ({ users, toggle, selectUser, getUsers }) => {
+	const deleteUser = (id) => {
+		axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`).then(() => getUsers())
+	}
+	return (
+		<div>
+			<h1>All users</h1>
+			<ul>
+				{users.map((user) => (
+					<li className='list' key={user.id}>
+						<div className='list-content'>
+							<div className='info'>
+								<h2>
+									{user.last_name} {user.first_name}
+								</h2>
+								<p>{user.email}</p>
+								<p>{user.birthday}</p>
+							</div>
+
+							<div className='btns'>
+								<button onClick={() => selectUser(user)}>
+									<i className='fa-solid fa-user-pen'></i>
+								</button>
+								<button className='delete' onClick={() => deleteUser(user.id)}>
+									<i className='fa-solid fa-trash'></i>
+								</button>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default UsersList
